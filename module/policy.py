675daa31976a84de518a72c20e6a9814040f4e9e
SionPark <rockgoat95@gmail.com>
2022-10-17 09:56:22 +0900

    add fast GLU

--- module/policy.py
+++ module/policy.py
@@ -7,11 +7,12 @@ from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class GLU(nn.Module):
     def __init__(self, size_in):
-        self.size_int = size_in
-        self.linear1 = nn.Linear(size_in,size_in)
-        self.linear2 = nn.Linear(size_in,size_in)
+        super().__init__()
+        self.size_in = size_in
+        self.linear = nn.Linear(size_in,size_in*2)
     def forward(self, X):
-        return (self.linear1(X) * self.linear2(X)).sigmoid()
+        out = self.linear(X)
+        return out[:,:self.size_in] * out[:,self.size_in:].sigmoid()
 
 class CustomPolicy(BaseFeaturesExtractor):
     """
@@ -26,13 +27,14 @@ class CustomPolicy(BaseFeaturesExtractor):
         # Re-ordering will be done by pre-preprocessing or wrapper
         n_input_channels = observation_space.shape[0]
         self.network = nn.Sequential(
-            nn.Linear(n_input_channels,200),
-            nn.LeakyReLU(),
-            nn.GLU(100),
-            nn.Linear(200,100),
-            nn.LeakyReLU(),
-            nn.GLU(100),
-            nn.Linear(100, action_dim)
+            nn.Linear(n_input_channels,128),
+            nn.LeakyReLU(0.1),
+            GLU(128),
+            nn.Linear(128,64),
+            nn.LeakyReLU(0.1),
+            GLU(64),
+            nn.Linear(64, action_dim),
+            nn.Sigmoid()
         )
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
@@ -40,6 +42,9 @@ class CustomPolicy(BaseFeaturesExtractor):
 
 policy_kwargs = dict(
     features_extractor_class=CustomPolicy,
-    features_extractor_kwargs=dict(action_dim = 8),
+    features_extractor_kwargs=dict(action_dim = 2),
 )
-model = PPO("CnnPolicy", "BreakoutNoFrameskip-v4", policy_kwargs=policy_kwargs, verbose=1)
+model = PPO("MlpPolicy", "CartPole-v1", policy_kwargs=policy_kwargs, verbose=1)
+
+model.learn(10)
+
